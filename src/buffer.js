71ffc995bf607c22f9d362fc81006962558d85eb
Sid van Vliet <sid@vvliet.me>
2019-04-12 15:22:40 +0200

    Converted to vanilla Js

--- src/buffer.js
+++ src/buffer.js
@@ -1,57 +1,42 @@
-if(!window.jQuery) { 
+document.addEventListener('DOMContentLoaded', function() {
 
-    console.error('You need jQuery enabled to use BufferJS. Next versions won\'t require jQuery.')
+    document.querySelectorAll('input[buffer-js]').forEach(function (item) {
 
-} else {
+        let buffer = 0
+        let drainSpeed = 1
+        let callback = null
 
-    $(document).ready(function () {
+        if(item.hasAttribute('buffer-js-delay'))
+            drainSpeed = item.getAttribute('buffer-js-delay')
 
-        $("input[buffer-js]").each(function(){
-            
-            let buffer = 0
-            let drain = 0
-            let drainSpeed = 1
-            let callback = null
-            
-            if($(this).attr('buffer-js-delay')){
-                drainSpeed = $(this).attr('buffer-js-delay')
-            }
+        if(item.hasAttribute('buffer-js-callback'))
+            callback = item.getAttribute('buffer-js-callback')
 
-            if($(this).attr('buffer-js-callback')){
-                callback = $(this).attr('buffer-js-callback')
-            }
-    
-            $(this).keypress(function () {
-                updateBuffer(100)
-            }).on('keydown', function(e) {
-                if (e.keyCode == 8) {
-                    updateBuffer(100)
-                }
-            })
-    
-            setInterval(function (callbackFunction) {
-                if( !buffer < 1 ) {
-                    if(buffer == 1)
-                    {
-                        fireCallback()
-                    }
-                    updateBuffer(buffer - 1)
-                }
-            }, drainSpeed * 10)
-
-            function fireCallback () {
-                const callbackFunction = eval( callback )
-                if(typeof callbackFunction == 'function'){
-                    callbackFunction()
-                }
-            }
-    
-            function updateBuffer (value) {
-                buffer = value
-            }
-            
+        item.addEventListener('keypress', function () {
+            updateBuffer(100)
         })
-    
+
+        item.addEventListener('keydown', function () {
+            updateBuffer(100)
+        })
+
+        setInterval(function (callbackFunction) {
+            if( !buffer < 1 )
+                if(buffer == 1)
+                    fireCallback()
+                updateBuffer(buffer - 1)
+        }, drainSpeed * 10)
+
+        function fireCallback () {
+            const callbackFunction = eval(callback)
+            if(typeof callbackFunction == 'function')
+                callbackFunction()
+        }
+
+        function updateBuffer (value) {
+            buffer = value
+        }
+
     })
 
-}
+})
